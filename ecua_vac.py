b6277073526dbc3c6ce727220b293bca6984e513
Galo <grivera92@gmail.com>
2021-04-16 12:29:50 +0200

    added time interval posting

--- ecua_vac.py
+++ ecua_vac.py
@@ -1,9 +1,21 @@
 import tweepy
+import time
 
 from auth import consumer_key,consumer_secret,key,secret
 
+# interval = 60 * 60 * 24  # every 24 hours
+interval = 15  # every 15 seconds, for testing
+
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
-
 api = tweepy.API(auth)
-api.update_status('posting from python test')
+
+
+count=1
+
+while True:
+    print("this is count number {}".format(count))
+    api.update_status('posting from python test tweet {}'.format(count))
+    count +=1
+    time.sleep(interval)
