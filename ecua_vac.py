ff04bb44076ef7ffffd7d3c0f4cb33b8b2e53b10
Galo <grivera92@gmail.com>
2021-04-19 17:25:37 +0200

    introduje diferentes escenarios de lo que pueda pasar

--- ecua_vac.py
+++ ecua_vac.py
@@ -20,20 +20,28 @@ future = datetime.date(2021,9,1)
 diff = future - today
 lim_dias=diff.days
 
-count=1
-
 while True:
     df=pd.read_csv("https://raw.githubusercontent.com/andrab/ecuacovid/master/datos_crudos/vacunas/vacunas.csv")
     fecha_rep=df["fecha"].iloc[-1]
     vaxs_1dosis=df["primera_dosis"].iloc[-1]
     vaxs_2dosis=df["segunda_dosis"].iloc[-1]
     left_2_vax= int(9e6-vaxs_1dosis)
-    if left_2_vax > 0:
+
+    #escenario1 en el que todavia tiene tiempo pero no acaba de vacunar
+    if (left_2_vax > 0 and lim_dias > 0):
         print('A Guillermo Lasso le quedan {} dias para vacunar {} personas. Hasta el {} el MSP ha reportado {} personas vacunadas con 1 dosis, {} personas con segunda dosis. Su ofrecimiento en campaña: 9M de vacunadxs en 100 dias'.format(lim_dias, left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis),flush=True)
         api.update_status('A Guillermo Lasso le quedan {} dias para vacunar {} personas. Hasta el {} el MSP ha reportado {} personas vacunadas con primera dosis, {} personas con segunda dosis. Su ofrecimiento en campaña: 9M de personas vacunadas en 100 dias #accountabilitybot #AI4good'.format(lim_dias, left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis))
         lim_dias -= 1
-        count +=1
         time.sleep(interval)
+
+    #escenario2 en el que se le acabo el tiempo. recordatorio de cuantos dias va sin cumplir so objetivo
+    if (left_2_vax > 0 and lim_dias <= 0):
+        print('Hace {} dias Guillermo Lasso debería haber vacunado 9M de personas y todavia le faltan {} personas para llegar a 9M. Hasta el {} el MSP ha reportado {} personas vacunadas con primera dosis, {} personas con segunda dosis.'.format(abs(lim_dias), left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis),flush=True)
+        api.update_status('Hace {} dias Guillermo Lasso debería haber vacunado 9M de personas y todavia le faltan {} personas para llegar a 9M. Hasta el {} el MSP ha reportado {} personas vacunadas con primera dosis, {} personas con segunda dosis.'.format(abs(lim_dias), left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis),flush=True)
+        lim_dias -= 1
+        time.sleep(interval)
+
+    #escenario3 en el que vacuna a 9M antes de que se acabe el tiempo
     if left_2_vax <= 0:
         print('Guillermo Lasso logró vacunar al menos 9M personas en sus primeros 100 dias de gobierno. Voy a buscar algo más que hacer. Chao'.format(lim_dias, left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis),flush=True)
         api.update_status('Guillermo Lasso logró vacunar al menos 9M personas en sus primeros 100 dias de gobierno. Voy a buscar algo más que hacer. Chao'.format(lim_dias, left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis))
