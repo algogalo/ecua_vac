19ebf356698fe373106500aa8032cf68cacecb67
Galo <grivera92@gmail.com>
2021-04-20 10:37:24 +0200

    removed unnecessary flushes

--- ecua_vac.py
+++ ecua_vac.py
@@ -31,7 +31,7 @@ while True:
     if (left_2_vax > 0 and lim_dias > 0):
         tweet='A G.Lasso le quedan {} días para vacunar {} personas. Hasta el {} el MSP ha reportado {} personas vacunadas con primera dosis, {} personas con segunda dosis. Su ofrecimiento en campaña: 9M de vacunadxs en 100 días'
         print(tweet.format(lim_dias, left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis),flush=True)
-        api.update_status(tweet.format(lim_dias, left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis),flush=True)
+        api.update_status(tweet.format(lim_dias, left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis))
         lim_dias -= 1
         time.sleep(interval)
 
@@ -39,14 +39,14 @@ while True:
     elif (left_2_vax > 0 and lim_dias <= 0):
         tweet='Hace {} días G.Lasso debería haber vacunado 9M de personas y todavia le faltan {} personas para llegar a 9M. Hasta el {} el MSP ha reportado {} personas vacunadas con primera dosis, {} personas con segunda dosis #accountabilitybot #AI4good'
         print(tweet.format(abs(lim_dias), left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis),flush=True)
-        api.update_status(tweet.format(abs(lim_dias), left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis),flush=True)
+        api.update_status(tweet.format(abs(lim_dias), left_2_vax, fecha_rep,vaxs_1dosis,vaxs_2dosis))
         lim_dias -= 1
         time.sleep(interval)
 
     #escenario3 en el que vacuna a 9M antes de que se acabe el tiempo
     elif left_2_vax <= 0:
         tweet='Guillermo Lasso logró vacunar al menos 9M personas en sus primeros 100 días de gobierno. Voy a buscar algo más que hacer. Chao #accountabilitybot #AI4good'
-        print(tweet)
+        print(tweet,flush=True)
         api.update_status(tweet)
         time.sleep(interval)
 
